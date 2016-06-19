002d41004dc974ee994711f4bd21828688eb3be1
Giovanni Lanotte <Giovanni Lanotte>
2016-06-19 19:23:41 +0200

    Aggiungo il progetto vuoto

---
+++ app/src/androidTest/java/it/uniba/di/ivs/sms16/giovannilanotte/scalettapreferita/ApplicationTest.java
@@ -0,0 +1,13 @@
+package it.uniba.di.ivs.sms16.giovannilanotte.scalettapreferita;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ app/src/main/java/it/uniba/di/ivs/sms16/giovannilanotte/scalettapreferita/MainActivity.java
@@ -0,0 +1,13 @@
+package it.uniba.di.ivs.sms16.giovannilanotte.scalettapreferita;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
